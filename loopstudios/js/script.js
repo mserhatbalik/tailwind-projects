7c0176637f76d7448cfb1faf8e4e0ae720b0e50f
Serhat <mserhatbalik@gmail.com>
2026-05-12 17:31:17 +0300

    Fixed the menu open close problem in medium and bigger screen sizes

--- loopstudios/js/script.js
+++ loopstudios/js/script.js
@@ -8,3 +8,12 @@ function navToggle() {
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
+
+// Reset menu state when screen size expands past the 'md' breakpoint (768px)
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    btn.classList.remove("open");
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
