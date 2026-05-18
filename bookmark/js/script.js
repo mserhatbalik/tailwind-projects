45bbaf49f2a157e974aa030bd31c2ad0fb0bbe5d
Serhat <mserhatbalik@gmail.com>
2026-05-18 17:27:52 +0300

    Added the hamburger menu and button with transitions

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -1,10 +1,16 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 // Tabs menu event listener
 tabs.forEach((tab) => {
   tab.addEventListener("click", onTabClick);
 });
+
+// Hamburger button listener
+btn.addEventListener("click", navToggle);
 
 function onTabClick(e) {
   // Deactivate all tabs
@@ -29,4 +35,16 @@ function onTabClick(e) {
     .classList.remove("hidden");
 
   //
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
